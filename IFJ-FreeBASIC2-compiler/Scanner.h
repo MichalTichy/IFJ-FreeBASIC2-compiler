d0169f6db2cd4fe70dec07b1ed986dac5d948617
Misanovy <misanovy@gmail.com>
2017-11-13 20:44:13 +0100

    Repair

---
+++ IFJ-FreeBASIC2-compiler/Scanner.h
@@ -0,0 +1,22 @@
+typedef enum {
+
+	S_Start,
+	S_Number,
+	S_ID,
+	S_Less,
+	S_Greater,
+	S_ExcString,
+	S_Dot,
+	S_Double,
+	S_DotDouble,
+	S_Exp,
+	S_ExpSign,
+
+}tState;
+
+void AddToString(int c, tToken *Token);
+void RemoveString(tToken *Token);
+void ConvertStringToInteger(tToken *Token);
+void ConvertStringToDouble(tToken *Token);
+TokenType CompareWithKeywords(char* string);
+tToken* GetNextToken(); 
