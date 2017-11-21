335133792d6ed9fb9e3a9ebfe74f1964dc289843
Michal Tichý <michal.tichy@riganti.cz>
2017-11-21 16:16:34 +0100

    Parser : IsInteger

--- IFJ-FreeBASIC2-compiler/Parser.h
+++ IFJ-FreeBASIC2-compiler/Parser.h
@@ -1,12 +1,21 @@
-typedef struct tNode
+#include "Basics.h"
+#include "Scanner.h"
+
+
+typedef struct NodeInteger
+{
+	int value;
+} tNodeInteger;
+
+typedef struct Node
 {
 	NodeType type;
 	union Data
 	{
-
-	};
-};
+		tNodeInteger* integer;
+	} tData;
+}tNode;
 
 typedef enum {
-	Test
+	Integer
 } NodeType;
