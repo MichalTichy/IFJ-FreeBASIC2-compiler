91282a02207d496effb6e188756412e28cb63d88
Michal Tichý <michal.tichy@riganti.cz>
2017-11-21 18:39:35 +0100

    parser

--- IFJ-FreeBASIC2-compiler/parser.h
+++ IFJ-FreeBASIC2-compiler/parser.h
@@ -1,9 +1,26 @@
-#pragma once
-#ifndef PARSER
-#define PARSER
-ERR_CODE Parse();
-ConstantTable_t * Gl_constant_table;
+#include "Basics.h"
+#include "Scanner.h"
+
+typedef enum NodeType {
+	Integer
+} NodeType;
+
+typedef struct NodeInteger
+{
+	int value;
+} tNodeInteger;
 
 
+typedef struct Node
+{
+	NodeType type;
+	union Data
+	{
+		tNodeInteger* integer;
+	} tData;
+}tNode;
 
-#endif // !PARSER
+tNode* IsNumber(tToken* token);
+tNode* IsInteger(tToken* token);
+tNode* InitNode(tToken* token);
+tNode* InitIntegerNode(int value);
