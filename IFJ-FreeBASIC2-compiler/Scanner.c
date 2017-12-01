8320e736f0751b54626cbf55dab1aa08ffd6b2da
Misanovy <misanovy@gmail.com>
2017-12-01 01:40:30 +0100

    Scanner BlockComment fix

--- IFJ-FreeBASIC2-compiler/Scanner.c
+++ IFJ-FreeBASIC2-compiler/Scanner.c
@@ -466,12 +466,14 @@ tToken* LoadToken()
 					{
 						c = (char) getchar();
 
-						if (c == '/' && c != EOF)
+						if (c == '/')
 						{
 							state = S_Start;
 							c = (char) tolower((char) getchar());
 							break;
 						}
+
+						return ScannerError(Token);
 					}
 					else if (c == '/')
 					{
