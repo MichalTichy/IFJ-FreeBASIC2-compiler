c80f1940ec7f8d277a152dd28642d58a45d93d87
Ondřej Deingruber <xdeing00@vutbr.cz>
2017-12-01 20:30:38 +0100

    functiontable

--- IFJ-FreeBASIC2-compiler/functiontable.c
+++ IFJ-FreeBASIC2-compiler/functiontable.c
@@ -1,22 +1,96 @@
 #include "functiontable.h"
 #include <stdlib.h>
+#include "errors.h"
+#include <string.h>
+#include "ManagedMalloc.h"
+#include "ADT.h"
 
 void FTInit(tFTItemPtr* tableptr)
 {
 
 }
 
-tFTItemPtr FTSearch(tFTItemPtr* tableptr, char* key)
+tFTItemPtr FTSearch(tFTItemPtr* tableptr, char* token)
 {
-	return NULL;
+	tFTItemPtr itemPtr = *tableptr;
+	while (itemPtr != NULL)
+	{
+		if (strcmp(itemPtr->data, token) == 0)
+		{
+			break;
+		}
+		else if (strcmp(itemPtr->data, token) < 0)		// Token is bigger
+		{
+			itemPtr = itemPtr->rptr;
+		}
+		else		// Token is smaller
+		{
+			itemPtr = itemPtr->lptr;
+		}
+	}
+	return itemPtr;
 }
 
-void FTInsert(tFTItemPtr* tableptr, char* key, char** parameters)
+void FTInsert(tFTItemPtr* tableptr, char* token, char** parameters)
 {
-
+	if (tableptr == NULL)
+	{
+		return;
+	}
+	tFTItemPtr* itemPtr = tableptr;
+	while ((*itemPtr) != NULL)
+	{
+		if (strcmp((*itemPtr)->data, token) == 0)			// Token is here
+		{
+			break;
+		}
+		else if (strcmp((*itemPtr)->data, token) < 0)		// Token is bigger
+		{
+			itemPtr = &((*itemPtr)->rptr);
+		}
+		else		// Token is smaller
+		{
+			itemPtr = &((*itemPtr)->lptr);
+		}
+	}
+	*itemPtr = mmalloc(sizeof(struct tFTItem));
+	if ((*itemPtr) == NULL)
+	{
+		exitSecurely(INTERNAL_ERR);
+	}
+	else
+	{
+		(*itemPtr)->len = (unsigned)strlen(token);
+		(*itemPtr)->data = token;
+		(*itemPtr)->parameters = parameters;
+		(*itemPtr)->lptr = NULL;
+		(*itemPtr)->rptr = NULL;
+	}
 }
 
 void FTFree(tFTItemPtr* tableptr)
 {
+	TStack_t s;
+	StackInit(&s);						// init stack of pointers
+	do
+	{
+		if ((*tableptr) == NULL)			// take node from stack
+		{
+			if (!StackIsEmpty(&s))
+			{
+				*tableptr = StackTopPop(&s);
+			}
+		}
+		else							// move right to stack
+		{
+			if ((*tableptr)->rptr != NULL)
+			{
+				StackPush(&s, (*tableptr)->rptr);
+			}
+			tFTItemPtr auxPtr = *tableptr;
+			*tableptr = (*tableptr)->lptr;		// go left
+			mfree(auxPtr);				// delete current node
+		}
 
+	} while (*tableptr != NULL || (!StackIsEmpty(&s)));
 }
--- IFJ-FreeBASIC2-compiler/functiontable.h
+++ IFJ-FreeBASIC2-compiler/functiontable.h
@@ -4,14 +4,14 @@ typedef struct tFTItem
 	char** parameters;
 	char* data;
 	unsigned int len;
-	struct tSTItem* lptr;
-	struct tSTItem* rptr;
+	struct tFTItem* lptr;
+	struct tFTItem* rptr;
 } *tFTItemPtr;
 
 void FTInit(tFTItemPtr* tableptr);
 
-tFTItemPtr FTSearch(tFTItemPtr* tableptr, char* key);
+tFTItemPtr FTSearch(tFTItemPtr* tableptr, char* token);
 
-void FTInsert(tFTItemPtr* tableptr, char* key, char** parameters);
+void FTInsert(tFTItemPtr* tableptr, char* token, char** parameters);
 
 void FTFree(tFTItemPtr* tableptr);
