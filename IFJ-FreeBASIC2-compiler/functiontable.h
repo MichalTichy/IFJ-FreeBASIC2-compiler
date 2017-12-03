d7eb787efbcf196ffdeb7c332e1604818e3a7ab3
Michal Tichý <michal.tichy@riganti.cz>
2017-12-03 22:30:14 +0100

    function table definition changed

--- IFJ-FreeBASIC2-compiler/functiontable.h
+++ IFJ-FreeBASIC2-compiler/functiontable.h
@@ -1,10 +1,16 @@
+#pragma once
+#ifndef FUNCTIONTABLE
+#define FUNCTIONTABLE
 
 #include "Scanner.h"
+#include "Parser.h";
+
 
 typedef struct tFTItem
 {
 	char** parameters;
 	char* data;
+	tFunction* body;
 	unsigned int len;
 	struct tFTItem* lptr;
 	struct tFTItem* rptr;
@@ -14,10 +20,11 @@ void FTInit(tFTItemPtr* tableptr);
 
 tFTItemPtr FTSearch(tFTItemPtr* tableptr, char* token);
 
-void AddParemeter(tFTItemPtr* funItem,char* name,ScalarType type);
-void AddReturnValue(tFTItemPtr* funItem,ScalarType type);
+void AddParemeter(tFTItemPtr funItem,char* name,ScalarType type);
+void AddReturnValue(tFTItemPtr funItem,ScalarType type);
 
-tFTItemPtr* FTInsert(tFTItemPtr* tableptr, char* token);
-tFTItemPtr* FTRemove(tFTItemPtr* funItem);
+struct tFTItem* FTInsert(tFTItemPtr* tableptr, char* token);
+void FTRemove(tFTItemPtr funItem);
 
 void FTFree(tFTItemPtr* tableptr);
+#endif
