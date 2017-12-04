0636d025eea5942c0c0c2f0dc0083c1431004af5
GabrielMastny <mastny.gabriel@gmail.com>
2017-12-05 00:06:09 +0100

    changed var asig rules

--- IFJ-FreeBASIC2-compiler/generator.c
+++ IFJ-FreeBASIC2-compiler/generator.c
@@ -236,14 +236,14 @@ void Recognize(struct Node* root, struct meta* metadata)
 			{
 				if (actualNode->tData.variable_assigment->Expression->tData.expression->expression->type == integerVal)
 				{
-					fprintf(stdout, "MOVE LF@_%s ", actualNode->tData.variable_assigment->id);
+					fprintf(stdout, "MOVE LF@%s ", actualNode->tData.variable_assigment->id);
 					Recognize(actualNode->tData.variable_assigment->Expression, met);
 					fprintf(stdout, "\n");
 				}
 				else if (actualNode->tData.variable_assigment->Expression->tData.expression->expression->type == expression)
 				{
 					Recognize(actualNode->tData.variable_assigment->Expression, met);
-					fprintf(stdout, "MOVE LF@_%s LF@_intVar%d\n", actualNode->tData.variable_assigment->id, met->intVarInUse);
+					fprintf(stdout, "MOVE LF@%s LF@_intVar%d\n", actualNode->tData.variable_assigment->id, met->intVarInUse);
 				}
 				else fprintf(stderr, "######### case var asig type Integer something elese ########");
 
@@ -253,14 +253,14 @@ void Recognize(struct Node* root, struct meta* metadata)
 			{
 				if (actualNode->tData.variable_assigment->Expression->tData.expression->expression->type == doubleVal)
 				{
-					fprintf(stdout, "MOVE LF@_%s ", actualNode->tData.variable_assigment->id);
+					fprintf(stdout, "MOVE LF@%s ", actualNode->tData.variable_assigment->id);
 					Recognize(actualNode->tData.variable_assigment->Expression, met);
 					fprintf(stdout, "\n");
 				}
 				else if (actualNode->tData.variable_assigment->Expression->tData.expression->expression->type == expression)
 				{
 					Recognize(actualNode->tData.variable_assigment->Expression, met);
-					fprintf(stdout, "MOVE LF@_%s LF@_doubleVar%d\n", actualNode->tData.variable_assigment->id, met->doubleVarInUse);
+					fprintf(stdout, "MOVE LF@%s LF@_doubleVar%d\n", actualNode->tData.variable_assigment->id, met->doubleVarInUse);
 				}
 				else fprintf(stderr, "######### case var asig type double something elese ########");
 			}
